2af5241a7df3d0a5d2b5cccb2e7ffa721279f74b
barel11 <barel57000@gmail.com>
2025-12-14 12:01:17 +0200

    dd

--- src/XO/Game.java
+++ src/XO/Game.java
@@ -29,7 +29,7 @@ public class Game {
 		 int counter = 0;
 		 for(int i = 0 ; i < 5 ; i++) {
 			 for(int j = 0 ; j < 5 ; j++) {
-				if(gameBoard[i][j] ! ) 
+				if(gameBoard[i][j] ! ) dddd
 			 }
 		 }
 		return  
